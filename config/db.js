e63a276831fc47f266c22d4228f30c64ca02312b
Ashutosh Pandey <lodhiarendra@gmail.com>
2023-04-11 11:50:08 +0530

    Major-Backend

---
+++ config/db.js
@@ -0,0 +1,20 @@
+import mongose from 'mongoose'
+import colors from 'colors'
+
+// a mongoose stuf (mongoose.connect ....) return always a promise
+const connectDB = async () => {
+    try{
+        console.log(process.env.MONGO_URI);
+        const conn = await mongose.connect(process.env.MONGO_URI,{
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true
+        })
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+    } catch (error) {
+      console.error(`Error: ${error.message}`.red.underline.bold)
+            process.exit(1)
+    }
+}
+
+export default connectDB
